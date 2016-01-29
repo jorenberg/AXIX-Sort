09b5231fca6992ae1e16fd34e6b3afe6b8a2d626
Prabhat Kumar <prabhat.genome@gmail.com>
2016-01-29 19:05:39 +0530

    Initial commit
    AXIX®-Sort, by Prabhat Kumar [http://prabhatkumar.org/].
    @license: Apache, version 2.0

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 #!/usr/bin/env node
+
+
+/*!
+ * AXIX®-Sort [v1.0.0]
+ * ______________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/]
+ * ______________________________________________________________
+ * @date      : 29-Jan-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * ______________________________________________________________
+ *
+ * --/The Heart of Core System/-- of "AXIX®-Sort".
+ * ______________________________________________________________
+ */
